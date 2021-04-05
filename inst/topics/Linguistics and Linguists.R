b2165120c7b0f97de74b8d4b92a2019515984920
ariaq <81649390+ariaq@users.noreply.github.com>
2021-04-05 21:46:26 +0800

    Linguistics and Linguists
    A bingo card for Linguistics Lover!

---
+++ inst/topics/Linguistics and Linguists.R
@@ -0,0 +1,28 @@
+# Linguistics and Linguists 
+# Author: Wenqing
+
+c(
+  "Phonology",
+  "Morphology",
+  "Vocal Tract",
+  "Noam Chomsky",
+  "Historical Linguistics",
+  "Sociolinguistics",
+  "Ferdinand de Saussure",
+  "Suffixes",
+  "Syntax",
+  "Psycolinguistics",
+  "Language acquisition",
+  "Place of Articulation",
+  "Edward Sapir",
+  "Phonetics",
+  "Otto Jespersen",
+  "Lyle Campbell",
+  "Approximants",
+  "Language Typology",
+  "Speech Pathology",
+  "Semantics",
+  "Garden Path Sentences",
+  "Computational Lingusitics",
+  "Grammaticalization",
+  "Categorical perception")
